e67b36d60c20bb40822a4cfa8612784afdd35dd7
Ruchik152 <info@nchernovn>
2024-08-20 19:27:40 +0300

    update

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/RuChik152/ftp_client
 
 go 1.19
 
-require github.com/jlaffaye/ftp v0.2.0
+require (
+	github.com/jlaffaye/ftp v0.2.0
+	github.com/joho/godotenv v1.5.1
+)
 
 require (
 	github.com/hashicorp/errwrap v1.0.0 // indirect
--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"bytes"
 	"crypto/tls"
 	"flag"
+	"fmt"
 	"io"
 	"log"
+	"os"
 	"time"
 
 	ftp "github.com/jlaffaye/ftp"
+	godotenv "github.com/joho/godotenv"
 )
 
 var path_file *string
@@ -17,14 +20,24 @@ func init() {
 
 	path_file = flag.String("path", "", "path /path/to/file")
 	flag.Parse()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 }
 
 func main() {
+
+	host := os.Getenv("SERVER")
+	port := os.Getenv("PORT")
+
 	tlsConfig := tls.Config{
-		InsecureSkipVerify: true, 
+		InsecureSkipVerify: true,
 	}
 
-	c, err := ftp.Dial("137.184.13.150:21", ftp.DialWithTimeout(5*time.Second), ftp.DialWithExplicitTLS(&tlsConfig))
+	c, err := ftp.Dial(fmt.Sprintf("%s:%s", host, port), ftp.DialWithTimeout(5*time.Second), ftp.DialWithExplicitTLS(&tlsConfig))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -34,18 +47,16 @@ func main() {
 		log.Fatal(err)
 	}
 
-	
-
 	data := bytes.NewReader(reader(*path_file))
 
-	err = c.Stor("./upload/my_test_44.txt", io.Reader(data)) 
+	err = c.Stor("./upload/my_test_44.txt", io.Reader(data))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-
-
 	if err := c.Quit(); err != nil {
 		log.Fatal(err)
 	}
+
+	os.Exit(0)
 }
