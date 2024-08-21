39fa52e46ea84f6be7ca9d2a97d4143ac687c503
Ruchik152 <info@nchernovn>
2024-08-21 12:36:39 +0300

    update tls version

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"crypto/tls"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"time"
@@ -15,10 +14,12 @@ import (
 )
 
 var path_file *string
+var name_file *string
 
 func init() {
 
 	path_file = flag.String("path", "", "path /path/to/file")
+	name_file = flag.String("name", "", "name name_you_file")
 	flag.Parse()
 
 	err := godotenv.Load()
@@ -35,27 +36,40 @@ func main() {
 
 	tlsConfig := tls.Config{
 		InsecureSkipVerify: true,
+		MinVersion:         tls.VersionTLS12,
+		MaxVersion:         tls.VersionTLS12,
 	}
 
-	c, err := ftp.Dial(fmt.Sprintf("%s:%s", host, port), ftp.DialWithTimeout(5*time.Second), ftp.DialWithExplicitTLS(&tlsConfig))
+	var c *ftp.ServerConn
+	var err error
+
+	for i := 0; i < 3; i++ {
+		c, err = ftp.Dial(fmt.Sprintf("%s:%s", host, port), ftp.DialWithTimeout(20*time.Second), ftp.DialWithExplicitTLS(&tlsConfig))
+		if err == nil {
+			break
+		}
+		log.Printf("Dial attempt %d failed: %v", i+1, err)
+		time.Sleep(5 * time.Second)
+	}
+
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Dial: ", err)
 	}
 
 	err = c.Login("ftp_karga", "413321337")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Login: ", err)
 	}
 
 	data := bytes.NewReader(reader(*path_file))
 
-	err = c.Stor("./upload/my_test_44.txt", io.Reader(data))
+	err = c.Stor(fmt.Sprintf("./upload/%s", *name_file), data)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Stor: ", err)
 	}
 
 	if err := c.Quit(); err != nil {
-		log.Fatal(err)
+		log.Fatal("Quit: ", err)
 	}
 
 	os.Exit(0)
